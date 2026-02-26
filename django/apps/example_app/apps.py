9ce5c51b060b19ef72eb69e10267bad775a70764
Richard <xurichard19@gmail.com>
2026-02-26 02:25:53 -0500

    worked on django folder

---
+++ django/apps/example_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ExampleAppConfig(AppConfig):
+    name = 'apps.example_app' # edited to include parent folder apps
---
+++ django/apps/example_app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+from .models import ExampleModel # we want access to our newly created model
+
+# Create your views here.
